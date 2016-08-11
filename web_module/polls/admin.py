f03a3e7e506f2714d8303148d0db303f7c7795ae
liuhanlong <liuhanlong3304@126.com>
2016-08-12 01:09:29 +0800

    add setup.py

--- web_module/polls/admin.py
+++ web_module/polls/admin.py
@@ -2,6 +2,24 @@ from django.contrib import admin
 
 
 # Register your models here.
-from .models import Question
+from .models import Question, Choice
 
-admin.site.register(Question)
+
+# class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    # fields = ['pub_date', 'question_text']
+    fieldsets = [
+        ('Question Text', {'fields': ['question_text']}),
+        ('Date Information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+admin.site.register(Question, QuestionAdmin)
--- web_module/polls/urls.py
+++ web_module/polls/urls.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
 # author: liuhanlong
 # date: 16/8/10 下午10:45
 from django.conf.urls import url
